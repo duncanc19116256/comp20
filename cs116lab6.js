03ed9a1650f8e4f995832e8e67354e3d06100235
duncanc19116256 <56744224+duncanc19116256@users.noreply.github.com>
2022-03-04 17:29:31 -0500

    Update cs116lab6.js

--- cs116lab6.js
+++ cs116lab6.js
@@ -1,3 +1,12 @@
-<script>
-  alert('hello world!');
-</script>
+function show_alert()
+{
+    alert("hihi");
+}
+
+$(document).ready(function() {
+    alert("I am an alert box2!");
+
+    $('.button').click(function() {
+        show_alert();
+    });
+});
